6d6e5c6d64c4a545e3dbb6a3538f09db476ff732
Nico <nico_204@hotmail.com>
2025-10-24 18:35:07 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,7 +16,9 @@ app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:3000'], // Vite
+  origin: ['http://localhost:3000',
+    'https://villanet-frontend.onrender.com'
+  ], // Vite
   credentials: true
 }));
 
